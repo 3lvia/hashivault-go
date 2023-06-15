cb913db09055525e94db2c82450135092de16c56
Ståle Heitmann <stale.heitmann@elvia.no>
2023-06-15 08:49:37 +0200

    Add auth method enumeration.

--- pkg/hashivault/options.go
+++ pkg/hashivault/options.go
@@ -2,6 +2,7 @@ package hashivault
 
 import (
 	"fmt"
+	"github.com/3lvia/hashivault-go/internal/auth"
 	"net/http"
 	"os"
 )
@@ -12,6 +13,8 @@ type optionsCollector struct {
 	gitHubToken  string
 	k8sMountPath string
 	k8sRole      string
+	useOIDC      bool
+	vaultToken   string
 }
 
 // Option is a function that can be used to configure this package.
@@ -21,6 +24,20 @@ type Option func(*optionsCollector)
 func WithClient(client *http.Client) Option {
 	return func(o *optionsCollector) {
 		o.client = client
+	}
+}
+
+// WithOIDC sets the authentication method to OIDC.
+func WithOIDC() Option {
+	return func(o *optionsCollector) {
+		o.useOIDC = true
+	}
+}
+
+// WithVaultToken sets the Vault token to use when authenticating to Vault.
+func WithVaultToken(token string) Option {
+	return func(o *optionsCollector) {
+		o.vaultToken = token
 	}
 }
 
@@ -46,6 +63,20 @@ func WithKubernetes(mountPath, role string) Option {
 	}
 }
 
+func (c *optionsCollector) authMethod() auth.Method {
+	if c.vaultToken != "" {
+		return auth.MethodToken
+	}
+	if c.useOIDC {
+		return auth.MethodOICD
+	}
+	if c.k8sMountPath != "" {
+		return auth.MethodK8s
+	}
+
+	return auth.MethodGitHub
+}
+
 func (c *optionsCollector) initialize() {
 	va := os.Getenv("VAULT_ADDR")
 	if va != "" {
@@ -66,11 +97,19 @@ func (c *optionsCollector) initialize() {
 	if k8sR != "" {
 		c.k8sRole = k8sR
 	}
+
+	vt := os.Getenv("VAULT_TOKEN")
+	if vt != "" {
+		c.vaultToken = vt
+	}
 }
 
 func (c *optionsCollector) validate() error {
 	if c.vaultAddress == "" {
 		return fmt.Errorf("VAULT_ADDR not set")
+	}
+	if c.useOIDC {
+		return nil
 	}
 	if c.gitHubToken == "" && c.k8sMountPath == "" {
 		return fmt.Errorf("GITHUB_TOKEN or MOUNT_PATH not set")
